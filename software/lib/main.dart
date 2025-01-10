3e66d8485f5bfdf7acc9a7eac78ff9e4503312b8
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-01-10 22:59:19 +0600

    hh

--- software/lib/main.dart
+++ software/lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'config/firebase_options.dart';
+import 'firebase_options.dart';
 // This was auto-generated
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
