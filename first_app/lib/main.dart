9c244f77bd0165affd2fddc288b8b9aa8f64202c
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-06-30 12:08:51 +0600

    6/30/25

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -20,27 +20,76 @@ class MyApp extends StatelessWidget{
 class HomeActivity extends StatelessWidget{
   const HomeActivity({super.key});
 
+  MySnackBar(message,context){
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content:Text(message))
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
+      //AppBar
       appBar: AppBar(
         title: Text("Inventory"),
-        titleSpacing: 0,
-        centerTitle: true,
+        titleSpacing: 20,
+        //centerTitle: true,
         toolbarHeight: 60,
         toolbarOpacity:1,
         elevation: 10,
-        backgroundColor: Colors.amber,
+        backgroundColor: Colors.green,
+        //set action
+        actions:[
+          IconButton(onPressed: (){MySnackBar("I am in Comments",context);}, icon: Icon(Icons.comment)),
+          IconButton(onPressed: (){MySnackBar("I am in Search",context);}, icon: Icon(Icons.search)),
+          IconButton(onPressed: (){MySnackBar("I am in Email Notifications",context);}, icon: Icon(Icons.email)),
+          IconButton(onPressed: (){MySnackBar("I am in  Settings",context);}, icon: Icon(Icons.settings)),
+          IconButton(onPressed: (){MySnackBar("I am in more option",context);}, icon: Icon(Icons.more_vert))
 
-      )
+        ],
+      ),
+
+      //floatingActionButton:()
+      floatingActionButton: FloatingActionButton(
+        elevation: 10,
+        child:Icon(Icons.add),
+        backgroundColor: Colors.green,
+        onPressed: (){
+          MySnackBar("I am in Floating Action Button",context);
+        },
+      ),
+
+      // bottomNavigationBar:(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 2,
+        backgroundColor: Colors.green,
+        items:[
+          BottomNavigationBarItem(icon: Icon(Icons.home),label:"Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.message),label:"Contact"),
+          BottomNavigationBarItem(icon: Icon(Icons.person),label:"Profile")
+        ],
+
+        onTap: (int index){
+        if(index==0){
+          MySnackBar("I am in Home",context);
+      }
+        if(index==1){
+          MySnackBar("I am in Contact",context);
+        }
+        if(index==2){
+          MySnackBar("I am in Profile",context);
+        }
+      },
+      ),
+
 
       //body: Text("Prosenjit Mondol is a legendary grandmaster at codeforces"),
       // drawer: (),
       // endDrawer:(),
-      // bottomNavigationBar:(),
-      // floatingActionButton:(),
+
+
     );
 
   }
