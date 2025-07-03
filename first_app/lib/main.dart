3ae8f368a1765194ec79a8b35145dac4479776e4
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-07-03 17:09:38 +0600

    7/3/25

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -11,13 +11,29 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home:const HomeActivity()
+        home: HomeActivity()
     );
   }
 }
 
 class HomeActivity extends StatelessWidget{
-  const HomeActivity({super.key});
+  HomeActivity({super.key});
+
+var MyItems=[
+  {"img":"https://pronad1.github.io/Personal-Portfolio/small-black.jpg","title":"Prosenjit"},
+  {"img":"https://pronad1.github.io/Personal-Portfolio/small-black.jpg","title":"Prosenjit"},
+  {"img":"https://pronad1.github.io/Personal-Portfolio/small-black.jpg","title":"Prosenjit"},
+  {"img":"https://pronad1.github.io/Personal-Portfolio/small-black.jpg","title":"Prosenjit"},
+  {"img":"https://pronad1.github.io/Personal-Portfolio/small-black.jpg","title":"Prosenjit"},
+  {"img":"https://pronad1.github.io/Personal-Portfolio/small-black.jpg","title":"Prosenjit"}
+
+
+];
+
+mySnackBar(context,msg){
+  return ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(msg)));
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +45,19 @@ class HomeActivity extends StatelessWidget{
         backgroundColor: Colors.green,
       ),
 
-
+      body: ListView.builder(
+        itemCount: MyItems.length,
+          itemBuilder: (context,index){
+          return GestureDetector(
+            onDoubleTap: (){mySnackBar(context, MyItems[index]['title']);},
+            child: Container(
+              margin: EdgeInsets.all(10),
+              height: 350,
+              child: Image.network(MyItems[index]['img']!, fit: BoxFit.fill,),
+            ),
+          );
+          },
+      )
     );
   }
 }
--- first_app/resourecs.dart
+++ first_app/resourecs.dart
@@ -257,3 +257,49 @@ Padding(padding: EdgeInsets.all(20),child: ElevatedButton(onPressed: (){},style:
 
 // Dynamic list view using json array
 // for that use GestureDetector , list view builder, JSOn array
+
+class HomeActivity extends StatelessWidget{
+HomeActivity({super.key});
+
+var MyItems=[
+{"img":"https://pronad1.github.io/Personal-Portfolio/small-black.jpg","title":"Prosenjit"},
+{"img":"https://pronad1.github.io/Personal-Portfolio/small-black.jpg","title":"Prosenjit"},
+{"img":"https://pronad1.github.io/Personal-Portfolio/small-black.jpg","title":"Prosenjit"},
+{"img":"https://pronad1.github.io/Personal-Portfolio/small-black.jpg","title":"Prosenjit"},
+{"img":"https://pronad1.github.io/Personal-Portfolio/small-black.jpg","title":"Prosenjit"},
+{"img":"https://pronad1.github.io/Personal-Portfolio/small-black.jpg","title":"Prosenjit"}
+
+
+];
+
+mySnackBar(context,msg){
+return ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(msg)));
+}
+
+
+@override
+Widget build(BuildContext context) {
+
+
+return Scaffold(
+appBar: AppBar(
+title: Text("My App"),
+backgroundColor: Colors.green,
+),
+
+body: ListView.builder(
+itemCount: MyItems.length,
+itemBuilder: (context,index){
+return GestureDetector(
+onDoubleTap: (){mySnackBar(context, MyItems[index]['title']);},
+child: Container(
+margin: EdgeInsets.all(10),
+height: 350,
+child: Image.network(MyItems[index]['img']!, fit: BoxFit.fill,),
+),
+);
+},
+)
+);
+}
+}
