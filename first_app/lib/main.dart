0999611657d696a3c984e52d5d268ec60d8feaea
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-07-11 07:55:48 +0600

    7/11/25

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -25,14 +25,104 @@ class HomeActivity extends StatelessWidget{
         title: Text("Prosen"),
         backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: CircularProgressIndicator(
-          color: Colors.green,
-          strokeWidth: 5,
-          backgroundColor: Colors.black,
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40)
+                  )
+              ),
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity1("This is from Activity 1")));
+              }, child: Text("Go Activity 1")),
+
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40)
+                  )
+              ),
+              onPressed: (){
+
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity2("This is from Activity 2")));
+              }, child: Text("Go Activity 2")),
+        ],
       ),
     );
 
+  }
+}
+
+class Activity1 extends StatelessWidget{
+
+  String msg;
+
+  Activity1(
+      this.msg,
+      {super.key}
+      );
+
+
+  @override
+  Widget build(BuildContext context){
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(msg),
+          backgroundColor: Colors.red,
+        ),
+        body:Center(
+          child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40)
+                  )
+              ),
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity2("This is from Activity 1 to Activity 2")));
+              }, child: Text("Go Activity 2")),
+        )
+    );
+  }
+}
+
+class Activity2 extends StatelessWidget{
+  String msg;
+  Activity2(
+      this.msg,
+      {super.key});
+
+
+  @override
+  Widget build(BuildContext context){
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(msg),
+          backgroundColor: Colors.pinkAccent,
+        ),
+        body:Center(
+          child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40)
+                  )
+              ),
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity1("This is from Activity 2 to Activity 1")));
+              }, child: Text("Go Activity 1")),
+        )
+    );
   }
 }
