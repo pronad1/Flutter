bf35eac74c067e6a7319ffa8968275423a269abb
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-07-28 01:53:47 +0600

    ok

--- taskmanager/lib/main.dart
+++ taskmanager/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Task Manager",
-      initialRoute: '/pinVerification',
+      initialRoute: '/login',
       routes: {
         '/':(context)=>splashScreen(),
         '/login':(context)=>loginScreen(),
