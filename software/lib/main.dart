fb8d9a4fe148e1525cd453e3ac20ddf0c85e8a54
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-08-06 01:26:31 +0600

    8/6/25

---
+++ software/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'config/firebase_options.dart';
+// This was auto-generated
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Reuse Hub',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Reuse Hub')),
+        body: Center(child: Text('Welcome to Reuse Hub')),
+      ),
+    );
+  }
+}
